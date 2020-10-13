0410071b34a9da58f4664f3b77db49de2214aaab
Mashnin Egor <y0g@mail.ru>
2020-10-13 19:39:03 +1000

    Attachment converted to sealed class Attachment types and added Post equals & hashCove overridden Post equals & hashCove test added

--- src/main/kotlin/ru/netology/attachment/Attachment.kt
+++ src/main/kotlin/ru/netology/attachment/Attachment.kt
@@ -1,5 +1,25 @@
 package ru.netology.attachment
 
-interface Attachment {
-    val type: AttachmentType
+import ru.netology.attachment.types.*
+
+sealed class Attachment{
+    data class AttachmentAudio (val audio: Audio){
+        val attachmentType = AttachmentType.AUDIO
+    }
+
+    data class AttachmentVideo (val video: Video){
+        val attachmentType = AttachmentType.VIDEO
+    }
+
+    data class AttachmentPhoto (val photo: Photo){
+        val attachmentType = AttachmentType.PHOTO
+    }
+
+    data class AttachmentDoc (val doc: Doc){
+        val attachmentType = AttachmentType.DOC
+    }
+
+    data class AttachmentLink (val link: Link){
+        val attachmentType = AttachmentType.LINK
+    }
 }
