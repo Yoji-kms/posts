b8dc2e5a1fae755a2671039ea980fb2a74c69a8b
Mashnin Egor <y0g@mail.ru>
2020-10-14 10:02:49 +1000

    AttachmentAudio..AttachmentLink made children of Attachment abstract val attachmentType added to Attachment

--- src/main/kotlin/ru/netology/attachment/Attachment.kt
+++ src/main/kotlin/ru/netology/attachment/Attachment.kt
@@ -3,23 +3,25 @@ package ru.netology.attachment
 import ru.netology.attachment.types.*
 
 sealed class Attachment{
-    data class AttachmentAudio (val audio: Audio){
-        val attachmentType = AttachmentType.AUDIO
+    abstract val attachmentType: AttachmentType
+
+    data class AttachmentAudio (val audio: Audio): Attachment() {
+        override val attachmentType = AttachmentType.AUDIO
     }
 
-    data class AttachmentVideo (val video: Video){
-        val attachmentType = AttachmentType.VIDEO
+    data class AttachmentVideo (val video: Video): Attachment(){
+        override val attachmentType = AttachmentType.VIDEO
     }
 
-    data class AttachmentPhoto (val photo: Photo){
-        val attachmentType = AttachmentType.PHOTO
+    data class AttachmentPhoto (val photo: Photo): Attachment(){
+        override val attachmentType = AttachmentType.PHOTO
     }
 
-    data class AttachmentDoc (val doc: Doc){
-        val attachmentType = AttachmentType.DOC
+    data class AttachmentDoc (val doc: Doc): Attachment(){
+        override val attachmentType = AttachmentType.DOC
     }
 
-    data class AttachmentLink (val link: Link){
-        val attachmentType = AttachmentType.LINK
+    data class AttachmentLink (val link: Link): Attachment(){
+        override val attachmentType = AttachmentType.LINK
     }
 }
